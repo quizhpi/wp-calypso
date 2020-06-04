908be3469b8e2e493b3044126aa28b001efabb13
Payton Swick <payton@foolord.com>
2020-06-04 16:41:21 -0400

    Use CalypsoI18nProvider in CompositeCheckout

--- client/my-sites/checkout/composite-checkout/composite-checkout.js
+++ client/my-sites/checkout/composite-checkout/composite-checkout.js
@@ -96,6 +96,7 @@ import { AUTO_RENEWAL } from 'lib/url/support';
 import { useLocalizedMoment } from 'components/localized-moment';
 import isDomainOnlySite from 'state/selectors/is-domain-only-site';
 import { retrieveSignupDestination, clearSignupDestinationCookie } from 'signup/utils';
+import { CalypsoI18nProvider } from 'components/calypso-i18n';
 
 const debug = debugFactory( 'calypso:composite-checkout' );
 
@@ -502,7 +503,7 @@ export default function CompositeCheckout( {
 	);
 
 	return (
-		<React.Fragment>
+		<CalypsoI18nProvider>
 			<PageViewTracker path={ analyticsPath } title="Checkout" properties={ analyticsProps } />
 			<CheckoutProvider
 				items={ itemsForCheckout }
@@ -544,7 +545,7 @@ export default function CompositeCheckout( {
 					isWhiteGloveOffer={ isWhiteGloveOffer }
 				/>
 			</CheckoutProvider>
-		</React.Fragment>
+		</CalypsoI18nProvider>
 	);
 }
 
