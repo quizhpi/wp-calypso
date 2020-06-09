bb0022588def11551e671bed8d8983e0fe66c21f
Payton Swick <payton@foolord.com>
2020-06-09 12:49:41 -0400

    Remove nextStepButtonText translations from WPCheckout

--- client/my-sites/checkout/composite-checkout/wpcom/components/wp-checkout.js
+++ client/my-sites/checkout/composite-checkout/wpcom/components/wp-checkout.js
@@ -293,7 +293,6 @@ export default function WPCheckout( {
 							titleContent={ <ContactFormTitle /> }
 							editButtonText={ translate( 'Edit' ) }
 							editButtonAriaLabel={ translate( 'Edit the contact details' ) }
-							nextStepButtonText={ translate( 'Continue' ) }
 							nextStepButtonAriaLabel={ translate( 'Continue with the entered contact details' ) }
 							validatingButtonText={
 								isCartPendingUpdate ? translate( 'Updating cart…' ) : translate( 'Please wait…' )
@@ -316,7 +315,6 @@ export default function WPCheckout( {
 						titleContent={ paymentMethodStep.titleContent }
 						editButtonText={ translate( 'Edit' ) }
 						editButtonAriaLabel={ translate( 'Edit the payment method' ) }
-						nextStepButtonText={ translate( 'Continue' ) }
 						nextStepButtonAriaLabel={ translate( 'Continue with the selected payment method' ) }
 						validatingButtonText={
 							isCartPendingUpdate ? translate( 'Updating cart…' ) : translate( 'Please wait…' )
